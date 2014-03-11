7cd1aeee54b2c7ebfa4ed50b4db486ce0e253dc7
Sergey Belash <sergey.belash@opencascade.com>
2014-03-11 11:43:40 +0400

    Restore CMAKE_BUILD_TYPE=Debug option, as OCC built in the debug mode

--- buildNewGEOM.bat
+++ buildNewGEOM.bat
@@ -7,5 +7,5 @@ call %SRC_DIR%\env.bat
 mkdir %ROOT_DIR%\build
 cd %ROOT_DIR%\build
 
-cmake %SRC_DIR% -G "Visual Studio 10" -DCMAKE_INSTALL_PREFIX:PATH=%ROOT_DIR%\install
+cmake %SRC_DIR% -G "Visual Studio 10" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:PATH=%ROOT_DIR%\install
 start "" %MSVC_EXE% NewGEOM.sln
