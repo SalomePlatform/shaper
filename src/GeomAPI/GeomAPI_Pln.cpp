9f142e599a01eef08c306f2852f5a95d7d6d483f
vsv <vitaly.smetannikov@opencascade.com>
2014-04-24 12:51:02 +0400

    std:shared_ptr replaced by boost::shared_ptr

--- src/GeomAPI/GeomAPI_Pln.cpp
+++ src/GeomAPI/GeomAPI_Pln.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-GeomAPI_Pln::GeomAPI_Pln(const shared_ptr<GeomAPI_Pnt>& thePoint,
-            const shared_ptr<GeomAPI_Dir>& theNormal)
+GeomAPI_Pln::GeomAPI_Pln(const boost::shared_ptr<GeomAPI_Pnt>& thePoint,
+            const boost::shared_ptr<GeomAPI_Dir>& theNormal)
 : GeomAPI_Interface(new gp_Pln(thePoint->impl<gp_Pnt>(),
                                theNormal->impl<gp_Dir>()))
 {
@@ -23,14 +23,14 @@ GeomAPI_Pln::GeomAPI_Pln(
 {
 }
 
-shared_ptr<GeomAPI_Pnt> GeomAPI_Pln::location()
+boost::shared_ptr<GeomAPI_Pnt> GeomAPI_Pln::location()
 {
   gp_Pnt aLoc = impl<gp_Pln>().Location();
-  return shared_ptr<GeomAPI_Pnt>(new GeomAPI_Pnt(aLoc.X(), aLoc.Y(), aLoc.Z()));
+  return boost::shared_ptr<GeomAPI_Pnt>(new GeomAPI_Pnt(aLoc.X(), aLoc.Y(), aLoc.Z()));
 }
 
-shared_ptr<GeomAPI_Dir> GeomAPI_Pln::direction()
+boost::shared_ptr<GeomAPI_Dir> GeomAPI_Pln::direction()
 {
   const gp_Dir& aDir = impl<gp_Pln>().Axis().Direction();
-  return shared_ptr<GeomAPI_Dir>(new GeomAPI_Dir(aDir.X(), aDir.Y(), aDir.Z()));
+  return boost::shared_ptr<GeomAPI_Dir>(new GeomAPI_Dir(aDir.X(), aDir.Y(), aDir.Z()));
 }
