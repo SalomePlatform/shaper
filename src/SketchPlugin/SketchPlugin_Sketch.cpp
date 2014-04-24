cf45387763e7aec06b955f71082eba8d65057b49
nds <natalia.donis@opencascade.com>
2014-04-24 12:42:31 +0400

    refs #30 - Sketch base GUI: create, draw lines
    Visualize three planes for sketch.

--- src/SketchPlugin/SketchPlugin_Sketch.cpp
+++ src/SketchPlugin/SketchPlugin_Sketch.cpp
@@ -5,6 +5,7 @@
 #include "SketchPlugin_Sketch.h"
 #include <ModelAPI_Data.h>
 #include <GeomAlgoAPI_FaceBuilder.h>
+#include <GeomAlgoAPI_CompoundBuilder.h>
 
 using namespace std;
 
@@ -26,11 +27,23 @@ void SketchPlugin_Sketch::execute()
 
 const boost::shared_ptr<GeomAPI_Shape>& SketchPlugin_Sketch::preview()
 {
-  boost::shared_ptr<GeomAPI_Pnt> anOrigin(new GeomAPI_Pnt(0, 0, 0));
-  boost::shared_ptr<GeomAPI_Dir> aNormal(new GeomAPI_Dir(1, 0, 0));
-  boost::shared_ptr<GeomAPI_Shape> aFace = 
-    GeomAlgoAPI_FaceBuilder::square(anOrigin, aNormal, PLANE_SIZE);
-  setPreview(aFace);
+  std::list<boost::shared_ptr<GeomAPI_Shape> > aFaces;
+
+  addPlane(1, 0, 0, aFaces); // YZ plane
+  addPlane(0, 1, 0, aFaces); // XZ plane
+  addPlane(0, 0, 1, aFaces); // XY plane
+  boost::shared_ptr<GeomAPI_Shape> aCompound = GeomAlgoAPI_CompoundBuilder::compound(aFaces);
+  setPreview(aCompound);
 
   return getPreview();
+}
+
+void SketchPlugin_Sketch::addPlane(double theX, double theY, double theZ,
+                                   std::list<boost::shared_ptr<GeomAPI_Shape> >& theShapes) const
+{
+  boost::shared_ptr<GeomAPI_Pnt> anOrigin(new GeomAPI_Pnt(0, 0, 0));
+  boost::shared_ptr<GeomAPI_Dir> aNormal(new GeomAPI_Dir(theX, theY, theZ));
+  boost::shared_ptr<GeomAPI_Shape> aFace = 
+    GeomAlgoAPI_FaceBuilder::square(anOrigin, aNormal, PLANE_SIZE);
+  theShapes.push_back(aFace);
 }
