975c481c4d5b3c2def28b6674b9c58e47f25ee95
nds <natalia.donis@opencascade.com>
2014-06-24 15:44:52 +0400

    refs #80 - Sketch base GUI: create/draw point, circle and arc
    Parallel, perpendicular features.

--- src/SketchPlugin/SketchPlugin_ConstraintLength.cpp
+++ src/SketchPlugin/SketchPlugin_ConstraintLength.cpp
@@ -4,6 +4,8 @@
 
 #include "SketchPlugin_ConstraintLength.h"
 
+#include <GeomDataAPI_Point2D.h>
+
 #include <ModelAPI_AttributeDouble.h>
 #include <ModelAPI_Data.h>
 
@@ -15,6 +17,7 @@ void SketchPlugin_ConstraintLength::initAttributes()
 {
   data()->addAttribute(CONSTRAINT_ATTR_VALUE,    ModelAPI_AttributeDouble::type());
   data()->addAttribute(CONSTRAINT_ATTR_FLYOUT_VALUE, ModelAPI_AttributeDouble::type());
+  data()->addAttribute(CONSTRAINT_ATTR_FLYOUT_VALUE_PNT, GeomDataAPI_Point2D::type());
   data()->addAttribute(CONSTRAINT_ATTR_ENTITY_A, ModelAPI_AttributeRefAttr::type());
 }
 
