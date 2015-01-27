456f3a5a34c59422d6016cb68417515920ed06fd
sbh <sergey.belash@opencascade.com>
2015-01-27 15:01:00 +0300

    Issue #385 issue with "thin" lines on debian resolved

---
+++ src/GeomAPI/GeomAPI_ICustomPrs.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2014-20xx CEA/DEN, EDF R&D
+
+// File:        GeomAPI_ICustomPrs.cpp
+// Created:     11 Dec 2014
+// Author:      Vitaly SMETANNIKOV
+
+#include <GeomAPI_ICustomPrs.h>
+
+GeomAPI_ICustomPrs::~GeomAPI_ICustomPrs()
+{
+
+}
