d1d44e75f69e0ac81c5981e05218e90515c07c46
vsv <vitaly.smetannikov@opencascade.com>
2015-05-12 17:50:19 +0300

    Make rigid constraint on vertexes of lines

--- src/SketcherPrs/SketcherPrs_Rigid.cpp
+++ src/SketcherPrs/SketcherPrs_Rigid.cpp
@@ -13,6 +13,8 @@
 #include <GeomAPI_Curve.h>
 #include <GeomAPI_Vertex.h>
 
+#include <ModelAPI_AttributeRefAttr.h>
+
 #include <SketchPlugin_Constraint.h>
 
 #include <AIS_Drawer.hxx>
@@ -47,13 +49,24 @@ SketcherPrs_Rigid::SketcherPrs_Rigid(ModelAPI_Feature* theConstraint,
 
 bool SketcherPrs_Rigid::updatePoints(double theStep) const 
 {
-  ObjectPtr aObj = SketcherPrs_Tools::getResult(myConstraint, SketchPlugin_Constraint::ENTITY_A());
-  if (SketcherPrs_Tools::getShape(aObj).get() == NULL)
-    return false;
+  std::shared_ptr<ModelAPI_Data> aData = myConstraint->data();
+  std::shared_ptr<ModelAPI_AttributeRefAttr> anAttr = aData->refattr(SketchPlugin_Constraint::ENTITY_A());
+  AttributePtr aRefAttr = anAttr->attr();
+  if (anAttr->isObject()) {
+    ObjectPtr aObj = anAttr->object();
+    if (SketcherPrs_Tools::getShape(aObj).get() == NULL)
+      return false;
 
-  SketcherPrs_PositionMgr* aMgr = SketcherPrs_PositionMgr::get();
-  gp_Pnt aP1 = aMgr->getPosition(aObj, this, theStep);
-  myPntArray->SetVertice(1, aP1);
+    SketcherPrs_PositionMgr* aMgr = SketcherPrs_PositionMgr::get();
+    gp_Pnt aP1 = aMgr->getPosition(aObj, this, theStep);
+    myPntArray->SetVertice(1, aP1);
+  } else {
+    std::shared_ptr<GeomAPI_Pnt2d> aPnt = SketcherPrs_Tools::getPoint(myConstraint, SketchPlugin_Constraint::ENTITY_A());
+    if (aPnt.get() == NULL)
+      return false;
+    std::shared_ptr<GeomAPI_Pnt> aPoint = myPlane->to3D(aPnt->x(), aPnt->y() + theStep);
+    myPntArray->SetVertice(1, aPoint->impl<gp_Pnt>());
+  }
   return true;
 }
 
@@ -61,7 +74,19 @@ bool SketcherPrs_Rigid::updatePoints(double theStep) const
 void SketcherPrs_Rigid::drawLines(const Handle(Prs3d_Presentation)& thePrs, Quantity_Color theColor) const
 {
   ObjectPtr aObj = SketcherPrs_Tools::getResult(myConstraint, SketchPlugin_Constraint::ENTITY_A());
-  std::shared_ptr<GeomAPI_Shape> aShape = SketcherPrs_Tools::getShape(aObj);
+  FeaturePtr aFeature = std::dynamic_pointer_cast<ModelAPI_Feature>(aObj);
+  std::shared_ptr<GeomAPI_Shape> aShape;
+  if (aFeature.get()) {
+    const std::list<ResultPtr>& aResults = aFeature->results();
+    std::list<ResultPtr>::const_iterator aIt;
+    for (aIt = aResults.cbegin(); aIt != aResults.cend(); aIt++) {
+      aShape = (*aIt)->shape();
+      if (aShape->isEdge())
+        break;
+    }
+  } else {
+    aShape = SketcherPrs_Tools::getShape(aObj);
+  }
   if (aShape.get() == NULL)
     return;
 
