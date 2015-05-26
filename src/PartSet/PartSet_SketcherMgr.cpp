b064f2ce47fc0c6bf35672bfc1397e9b7c121d3a
vsv <vitaly.smetannikov@opencascade.com>
2015-05-26 11:55:20 +0300

    Can display method can not change displaying property

--- src/PartSet/PartSet_SketcherMgr.cpp
+++ src/PartSet/PartSet_SketcherMgr.cpp
@@ -843,10 +843,6 @@ bool PartSet_SketcherMgr::canDisplayObject(const ObjectPtr& theObject) const
       }
     }
   }
-  if (!isObjectFound) {
-    theObject->setDisplayed(aCanDisplay); // If it be set into True - set to False
-    return aCanDisplay;
-  }
 
   // 4. For created nested feature operation do not display the created feature if
   // the mouse curstor leaves the OCC window.
