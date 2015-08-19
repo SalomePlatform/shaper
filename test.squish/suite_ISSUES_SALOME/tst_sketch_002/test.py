4b8b2c051a85f3a8ac3e2f2bc2fc456b6e353f45
eso <eugeny.savchenko@opencascade.com>
2015-08-19 13:49:24 +0300

    Add new tests

---
+++ test.squish/suite_ISSUES_SALOME/tst_sketch_002/test.py
@@ -0,0 +1,52 @@
+def sketch():
+    #[step] Click 'Create sketch' toolbar button
+    #[step] Select XY plane for sketching
+    #[step] Create random closed contour
+    line_create_in_view((101, 183),(265, 107))
+    line_create_in_view((265, 107), (520, 192))
+    line_create_in_view((520, 192), (443, 411))
+    closing_line_create_in_view((443, 411), (101, 183))
+    
+    #[step] Create one more closed contour inside previous, not parallel to it
+    line_create_in_view((219, 198),(269, 166))
+    line_create_in_view((269, 166), (451, 230))
+    line_create_in_view((451, 230), (410, 334))
+    closing_line_create_in_view((410, 334), (219, 199))
+    
+    #[check] Check that lines are not parallel and there are no icons near lines
+    test.vp("VP1")
+    #[step] Make according lines parallel
+    parallel([(394, 151), (361, 199)])
+    parallel([(201, 137), (244, 182)])
+    parallel([(245, 278), (293, 250)])
+    parallel([(430, 286), (483, 300)])
+    # [check] Check that lines are parallel and there are icons near lines
+    test.vp("VP2")
+
+def main():
+    # [project] NewGEOM
+    #[Scenario] Sketch_002
+    #[Topic] Parallel objects
+    #[Tested functionality]Parallelism between main lines
+    
+    source(findFile("scripts", "common.py"))
+
+    #[section] Application start
+    #[step] Launch SALOME
+    startApplication("salome_run.sh")
+
+    set_defaults()
+    
+    #[step] Activate NewGeom
+    #[step]Click 'New' button
+    activate_newgeom()
+    #[section] Creation of main lines and make them parallel
+    
+    sketch_create(help_points("XY_plane"), lambda: sketch())
+    #[step] Save study
+    save("sketch_002")
+    clickButton(waitForObject(":Warning.Yes_QPushButton"))
+    ##[step] Close application
+    sendEvent("QCloseEvent", waitForObject(":SALOME*_STD_TabDesktop"))
+    clickButton(waitForObject(":Exit.Shutdown servers_QCheckBox"))
+    clickButton(waitForObject(":Exit.Ok_QPushButton"))
---
+++ test.squish/suite_ISSUES_SALOME/tst_sketch_003/test.py
@@ -0,0 +1,67 @@
+def sketch():
+    circle_create_in_view((302, 221), (436, 287))
+    
+def main():
+    #[project] NewGEOM
+    #[Scenario] Sketch_003
+    #[Topic]
+    #[Tested functionality] Saving and opening studies
+    #[Summary description]
+    #[Expected results]
+    #[General comments]
+    
+    source(findFile("scripts", "common.py"))
+    
+    #[section] Application start
+    #[step] Launch SALOME
+    startApplication("salome_run.sh")
+
+    set_defaults()
+    #[section] Creation of 3D model
+    #[step] Activate NewGeom
+    #[step]Click 'New' button
+    
+    activate_newgeom()
+    #[step] Create new part
+    part_create()
+    #[step] Create sketch, consisting from one circle
+    sketch_create(help_points("XZ_plane"), lambda: sketch())
+    
+    #[step] Execute extrusion. Use parameters to size = 100 and from size = 50
+    extrusion((182, 140), 100, 50)
+    
+    #[step] Rotate obtained model
+    type(waitForObject(":SALOME*.3D View Operations_OCCViewer_ViewPort3d"), "<Control>")
+    mouseDrag(waitForObject(":SALOME*.3D View Operations_OCCViewer_ViewPort3d"), 425, 422, 20, -23, 67108865, Qt.LeftButton)
+    type(waitForObject(":SALOME*.3D View Operations_OCCViewer_ViewPort3d"), "<Control>")
+    mouseDrag(waitForObject(":SALOME*.3D View Operations_OCCViewer_ViewPort3d"), 446, 411, -13, -137, 67108866, Qt.RightButton)
+    test.vp("VP1")
+
+    #[section] Save file
+    #[step] File - Save as
+    #[step] 'Save as' dialog appears
+    #[step] Type name of file
+    #[step] Click 'Save' button    
+    save("sketch_003")
+    clickButton(waitForObject(":Warning.Yes_QPushButton"))
+    
+    #[step] Close document
+    clickButton(waitForObject(":SALOME*.Close_QToolButton"))
+    
+    #[step] File - Open 
+    #[step} Open just saved file  
+    open("sketch_003.hdf")
+    #[step] Activate NewGeom
+    clickButton(waitForObject(":SALOME 7.6.0.NewGeom_QToolButton"))
+    
+
+    #[step] Activate Part_1
+    waitForObjectItem(":Object browser_XGUI_DataTree_2", "Part\\_1")
+    clickItem(":Object browser_XGUI_DataTree_2", "Part\\_1", 50, 11, 0, Qt.LeftButton)
+    openItemContextMenu(waitForObject(":Object browser_XGUI_DataTree_2"), "Part\\_1", 50, 11, 0)
+    activateItem(waitForObjectItem(":_QMenu", "Activate"))
+    
+    #[check] Check that file was loaded correct
+    test.vp("VP2")
+    #[step] Close application
+    close_application()
