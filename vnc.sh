8e854a817158c0f1d90a3285c1c372ce22c0f0f6
spo <sergey.pokhodenko@opencascade.com>
2015-08-20 16:09:58 +0300

    Modify vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 
+if [ -z ${DISPLAY_PORT} ]; then
+  export DISPLAY_PORT=99
+fi
+
+echo "DISPLAY is :${DISPLAY_PORT}"
+
 export PATH=/dn23/NEWGEOM/NEWGEOM_JENKINS_BUILD_AREA/tools:/opt/TurboVNC/bin:${PATH}
-./vnc-run vglrun "$@"
+./vnc-run --server-num ${DISPLAY_PORT} vglrun "$@"
