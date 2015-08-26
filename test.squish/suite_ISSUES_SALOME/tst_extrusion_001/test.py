7c06295aea6523048b676312fbeb5da673c0659d
eso <eugeny.savchenko@opencascade.com>
2015-08-26 11:45:55 +0300

    Add new test

--- test.squish/suite_ISSUES_SALOME/tst_extrusion_001/test.py
+++ test.squish/suite_ISSUES_SALOME/tst_extrusion_001/test.py
@@ -1,7 +1,7 @@
 def main():
     #[project] NewGEOM
     #[Scenario] Extrusion_001
-    #[Topic] 'Extrusion' functionality
+    #[Topic] 'Extrusion from size to size' functionality
     #[Tested functionality] 
     #[Summary description]
     #[Expected results]
@@ -19,32 +19,31 @@ def main():
     open(DATA_PATH + "/for_extrusion_001.hdf")
     
     #[step] Activate NewGeom
-    clickButton(waitForObject(":SALOME 7.6.0.NewGeom_QToolButton"))
+    clickButton(waitForObject(":SALOME*.NewGeom_QToolButton"))
     
-    #[step] Activate Part_1   
-    openItemContextMenu(waitForObject(":Object browser_XGUI_DataTree_2"), "Part\\_1", 44, 10, 0)
+    #[step] Activate Part_1
+    waitForObjectItem(":Object browser_XGUI_DataTree", "Part\\_1")
+    clickItem(":Object browser_XGUI_DataTree", "Part\\_1", 48, 10, 0, Qt.LeftButton)
+    openItemContextMenu(waitForObject(":Object browser_XGUI_DataTree"), "Part\\_1", 48, 10, 0)
     activateItem(waitForObjectItem(":_QMenu", "Activate"))
     
-    #[step] Execute extrusion by sizes, use Sketch_001, 50 as to size and 10 as from size 
+    #[step] Execute extrusion for sketch_1
     mouseClick(waitForObjectItem(":SALOME*_QMenuBar", "Features"))
     mouseClick(waitForObjectItem(":_QMenu", "Extrusion"))
-    mouseClick(waitForObject(":SALOME*.3D View Operations_OCCViewer_ViewPort3d"), 345, 226, 0, Qt.LeftButton)
-    mouseClick(waitForObject(":Extrusion.qt_spinbox_lineedit_QLineEdit"), 25, 5, 0, Qt.LeftButton)
+    mouseClick(waitForObject(":SALOME*.3D View Operations_OCCViewer_ViewPort3d"), 347, 228, 0, Qt.LeftButton)
+
     type(waitForObject(":Extrusion.to_size_ModuleBase_ParamSpinBox"), "<Ctrl+A>")
-    type(waitForObject(":Extrusion.to_size_ModuleBase_ParamSpinBox"), "<Keypad_5>")
-    type(waitForObject(":Extrusion.to_size_ModuleBase_ParamSpinBox"), "<Keypad_0>")
-    mouseClick(waitForObject(":Extrusion.qt_spinbox_lineedit_QLineEdit_2"), 21, 9, 0, Qt.LeftButton)
+    type(waitForObject(":Extrusion.to_size_ModuleBase_ParamSpinBox"), 50)
+
     type(waitForObject(":Extrusion.from_size_ModuleBase_ParamSpinBox"), "<Ctrl+A>")
-    type(waitForObject(":Extrusion.from_size_ModuleBase_ParamSpinBox"), "<Keypad_1>")
-    type(waitForObject(":Extrusion.from_size_ModuleBase_ParamSpinBox"), "<Keypad_0>")
+    type(waitForObject(":Extrusion.from_size_ModuleBase_ParamSpinBox"), 10)
     
-    #[check] Check that preview appears
-    test.vp("VP1")
-    
+    #[check] Check that preview is updated
+    test.vp("VP3")
     clickButton(waitForObject(":Extrusion.property_panel_ok_QToolButton"))
     
     #[check] Check that extrusion has been implemented
-    test.vp("VP2")
+    test.vp("VP4")
     
     # [step] Close application without saving
     close_application()
--- test.squish/suite_ISSUES_SALOME/tst_sketch_011/test.py
+++ test.squish/suite_ISSUES_SALOME/tst_sketch_011/test.py
@@ -1,6 +1,4 @@
 
-
-
 def main():
     #[project] NewGEOM
     #[Scenario] Sketch_010
