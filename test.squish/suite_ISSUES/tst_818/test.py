9c1ef57e84765cd93329f3b40b116b803f3d43b7
spo <sergey.pokhodenko@opencascade.com>
2015-09-01 17:44:44 +0300

    Annotate suite_ISSUES/tst_818

--- test.squish/suite_ISSUES/tst_818/test.py
+++ test.squish/suite_ISSUES/tst_818/test.py
@@ -4,10 +4,13 @@ def main():
     startApplication("linux_run.sh")
     set_defaults()
     
+    #[step] Create parameter 'a = 100'
     parameter_create("a", "100")
     
+    #[step] Create sketch with line (0,0) - (a, 0)
     sketch_create(help_points("XY_plane"), lambda: line_create((0, 0), ("a", 0)))
 
+    #[step] Activate context menu for parameter 'a' and click 'Delete'
     waitForObjectItem(":Object browser_XGUI_DataTree", "Parameters (1)")
     clickItem(":Object browser_XGUI_DataTree", "Parameters (1)", -10, 10, 0, Qt.LeftButton)
     waitForObjectItem(":Object browser_XGUI_DataTree", "Parameters (1).a = 100")
@@ -16,7 +19,9 @@ def main():
     
     activateItem(waitForObjectItem(":_QMenu", "Delete"))
     
+    #[check] Message box with "Selected features are used in the following features: SketchLine_1. These features will be deleted.\nAlso these features will be deleted: Sketch_1.\nWould you like to continue?" should appear
     waitFor("object.exists(':Delete features.Label_QLabel')", 20000)
     test.compare(str(findObject(":Delete features.Label_QLabel").text), "Selected features are used in the following features: SketchLine_1. These features will be deleted.\nAlso these features will be deleted: Sketch_1.\nWould you like to continue?")
 
+    #[step] Click 'Yes' and close application
     clickButton(waitForObject(":Delete features.Yes_QPushButton"))
