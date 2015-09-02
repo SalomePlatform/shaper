3a5c23a0ee5c1673633fee0e6ea2c557cedd0dc9
spo <sergey.pokhodenko@opencascade.com>
2015-09-02 09:39:44 +0300

    Fix suite_ISSUES/tst_PARALLEL_2

--- test.squish/suite_ISSUES/tst_PARALLEL_2/test.py
+++ test.squish/suite_ISSUES/tst_PARALLEL_2/test.py
@@ -1,17 +1,22 @@
 def sketch():
-    #line creation
+    #[step] Create lines [vp VP_SKETCH]
     clickButton(waitForObject(":Basic.Line_AppElements_Button"))
     mouseClick(waitForObject(":OpenParts*_AppElements_ViewPort"), 123, 417, 0, Qt.LeftButton)
     mouseClick(waitForObject(":OpenParts*_AppElements_ViewPort"), 175, 132, 0, Qt.LeftButton)
     mouseClick(waitForObject(":OpenParts*_AppElements_ViewPort"), 477, 63, 0, Qt.LeftButton)
     mouseClick(waitForObject(":OpenParts*_AppElements_ViewPort"), 739, 397, 0, Qt.LeftButton)
-    mouseClick(waitForObject(":OpenParts*_AppElements_ViewPort"), 222, 334, 0, Qt.LeftButton)
-    type(waitForObject(":OpenParts*_AppElements_ViewPort"), "<Esc>")
+    mouseClick(waitForObject(":OpenParts*_AppElements_ViewPort"), 123, 417, 0, Qt.LeftButton)
+    test.vp("VP_SKETCH")
     
-    parallel_create((165, 190), (581, 197))
-    parallel_create((285, 135), (469, 364))
-    #[check] Check that lines are parallel
-    test.vp("VP1")
+    #[step] Create 2 lines parallel
+    parallel_create((355, 91), (445, 407))
+    # [check] Check that lines are parallel [vp VP_PARALLEL_1]
+    test.vp("VP_PARALLEL_1")
+    
+    #[step] Create 2 other lines parallel
+    parallel_create((155, 225), (589, 216))
+    #[check] Check that lines are parallel [vp VP_PARALLEL_2]
+    test.vp("VP_PARALLEL_2")
 
 def main():
     source(findFile("scripts", "common.py"))
