e06ae86b96c69761e7acf8074908dda3d0e6481b
vsr <vsr@opencascade.com>
2015-10-21 13:45:08 +0300

    Fix problem with make test when environment is not set

--- CMakeCommon/UnitTest.cmake
+++ CMakeCommon/UnitTest.cmake
@@ -26,6 +26,9 @@
 ENABLE_TESTING()
 
 MACRO(ADD_UNIT_TESTS)
+  SET(_LD_LIBRARY_PATH "${CMAKE_INSTALL_PREFIX}/bin:${CMAKE_INSTALL_PREFIX}/swig:${CMAKE_INSTALL_PREFIX}/plugins:${SUIT_LIB_DIR}:$ENV{LD_LIBRARY_PATH}")
+  SET(_PYTHONPATH "${CMAKE_INSTALL_PREFIX}/swig:${CMAKE_INSTALL_PREFIX}/plugins:${CMAKE_INSTALL_PREFIX}/addons:${CMAKE_INSTALL_PREFIX}/pythonAPI:$ENV{PYTHONPATH}")
+  SET(_CONFIG_FILE "${CMAKE_INSTALL_PREFIX}/plugins")
   foreach(eachFileName ${ARGN})
     # Strip the ".py" suffix 
     GET_FILENAME_COMPONENT(aTestName ${eachFileName} NAME_WE)
@@ -39,6 +42,8 @@ MACRO(ADD_UNIT_TESTS)
     IF(EXISTS ${aTestFileName})
       ADD_TEST(NAME ${aTestName}
                COMMAND ${PYTHON_EXECUTABLE} ${aTestFileName})
+      SET_TESTS_PROPERTIES(${aTestName} PROPERTIES
+               ENVIRONMENT "LD_LIBRARY_PATH=${_LD_LIBRARY_PATH};PYTHONPATH=${_PYTHONPATH};NEW_GEOM_CONFIG_FILE=${_CONFIG_FILE}")
       # Debug output...
       #MESSAGE(STATUS "Test added: ${aTestName} file: ${aTestFileName}")
     ELSE(EXISTS ${aTestFileName})
