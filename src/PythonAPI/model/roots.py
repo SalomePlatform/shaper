9d42cb67c174056ada8ca5e9c335af61ad14c3ff
spo <sergey.pokhodenko@opencascade.com>
2015-10-22 10:28:40 +0300

    Make services.py and roots.py more PEP8 complience and add reset() function

--- src/PythonAPI/model/roots.py
+++ src/PythonAPI/model/roots.py
@@ -3,39 +3,44 @@ Author: Daniel Brunier-Coulin
 Copyright (C) 2014-20xx CEA/DEN, EDF R&D
 """
 
-from ModelAPI import *
+import ModelAPI
 
 
-class Feature(ModelAPI_Feature):
-  """Base class of user-defined Python features."""
 
-  def __init__(self):
-    ModelAPI_Feature.__init__(self)
+class Feature(ModelAPI.ModelAPI_Feature):
+    """Base class of user-defined Python features."""
 
-  def addRealInput (self, inputid):
-    self.data().addAttribute(inputid, ModelAPI_AttributeDouble_typeId())
+    def __init__(self):
+        ModelAPI.ModelAPI_Feature.__init__(self)
 
-  def getRealInput (self, inputid):
-    return self.data().real(inputid).value()
+    def addRealInput (self, inputid):
+        self.data().addAttribute(inputid,
+                                 ModelAPI.ModelAPI_AttributeDouble_typeId())
 
-  def addResult (self, result):
-    shape = result.shape()
-    body  = self.document().createBody( self.data() )
-    body.store(shape)
-    self.setResult(body)
+    def getRealInput (self, inputid):
+        return self.data().real(inputid).value()
+
+    def addResult (self, result):
+        shape = result.shape()
+        body = self.document().createBody(self.data())
+        body.store(shape)
+        self.setResult(body)
 
 
 class Interface():
-  """Base class of hight level Python interfaces to features."""
+    """Base class of hight level Python interfaces to features."""
 
-  def __init__(self, container, fid):
-    self.my = container.addFeature(fid)
+    def __init__(self, feature):
+        self._feature = feature
 
-  def setRealInput (self, inputid, value):
-    self.my.data().real(inputid).setValue(value)
 
-  def areInputValid (self):
-    return ModelAPI_Session.get().validators().validate(self.my)
 
-  def execute (self):
-    self.my.execute()
+    def setRealInput (self, inputid, value):
+        self._feature.data().real(inputid).setValue(value)
+
+    def areInputValid (self):
+        validators = ModelAPI.ModelAPI_Session.get().validators()
+        return validators.validate(self._feature)
+
+    def execute (self):
+        self._feature.execute()
