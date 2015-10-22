7af987ce01755058718fb1ece59b50218a9dad07
spo <sergey.pokhodenko@opencascade.com>
2015-10-22 09:57:24 +0300

    Make addBox as a function.

--- src/PythonAPI/extension/box.py
+++ src/PythonAPI/extension/box.py
@@ -7,17 +7,22 @@ import model
 from macros.box.feature import BoxFeature as MY
 
 
+def addBox(self, container, *args):
+    feature = container.addFeature(MY.ID())
+    return Box(feature, *args)
+
+
 class Box(model.Interface):
-  """Executes the macro-feature Box.
-  """
-  def __init__(self, part, dx, dy, dz):
-    model.Interface.__init__(self, part, MY.ID())
+    """Executes the macro-feature Box."""
 
-    self.setRealInput( MY.WIDTH_ID(), dx )
-    self.setRealInput( MY.LENGTH_ID(), dy )
-    self.setRealInput( MY.HEIGHT_ID(), dz )
+    def __init__(self, feature, dx, dy, dz):
+        model.Interface.__init__(self, feature)
 
-    if self.areInputValid():
-      self.execute()
-    else:
-      raise Exception("cannot make the Box")
+        self.setRealInput(MY.WIDTH_ID(), dx)
+        self.setRealInput(MY.LENGTH_ID(), dy)
+        self.setRealInput(MY.HEIGHT_ID(), dz)
+
+        if self.areInputValid():
+            self.execute()
+        else:
+            raise Exception("cannot make the Box")
