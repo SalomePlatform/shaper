592b8a38b3c9e2a6a7d3d1d180b1f9b5406c4415
spo <sergey.pokhodenko@opencascade.com>
2015-10-30 15:11:29 +0300

    Fix SketcherSetEqual.test_length_equality

--- src/PythonAPI/Test/TestSketcherSetEqual.py
+++ src/PythonAPI/Test/TestSketcherSetEqual.py
@@ -4,7 +4,7 @@ import math
 import TestSketcher
 from TestSketcher import SketcherTestCase
 
-class SketcherSetEqual(SketcherTestCase):   
+class SketcherSetEqual(SketcherTestCase):
     def test_length_equality(self):
         # Set the constraint
         l1 = self.sketch.addLine(0, 0, 0, 1)
@@ -14,15 +14,15 @@ class SketcherSetEqual(SketcherTestCase):
         model.do()
         # Check the result
         length_1 = math.sqrt(
-            math.pow((l1.endPointData().x() - l1.startPointData().x()), 2) + 
+            math.pow((l1.endPointData().x() - l1.startPointData().x()), 2) +
             math.pow((l1.endPointData().y() - l1.startPointData().y()), 2)
             )
         length_2 = math.sqrt(
-            math.pow((l1.endPointData().x() - l1.startPointData().x()), 2) + 
+            math.pow((l2.endPointData().x() - l2.startPointData().x()), 2) +
             math.pow((l2.endPointData().y() - l2.startPointData().y()), 2)
             )
         self.assertAlmostEqual(length_1, length_2, delta=TestSketcher.DELTA)
-        
+
     def test_radius_equality(self):
         # Set the constraint
         circle_1 = self.sketch.addCircle(0, 0, 10.0)
@@ -32,11 +32,11 @@ class SketcherSetEqual(SketcherTestCase):
         model.do()
         # Check the result
         self.assertAlmostEqual(
-            circle_1.radiusData().value(), 
-            circle_2.radiusData().value(), 
+            circle_1.radiusData().value(),
+            circle_2.radiusData().value(),
             delta=TestSketcher.DELTA
             )
-        
+
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(SketcherSetEqual)
     unittest.TextTestRunner(verbosity=2).run(suite)
