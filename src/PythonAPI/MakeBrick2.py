6b0b6b57892a7eaeac32b0a11e2e729087b0f120
spo <sergey.pokhodenko@opencascade.com>
2015-10-30 15:09:08 +0300

    Add TestMakeBrick2.py

--- src/PythonAPI/MakeBrick2.py
+++ src/PythonAPI/MakeBrick2.py
@@ -19,51 +19,51 @@ mypart = model.addPart(mypartset).document()
 
 # Creating the base of the box
 
-mybase = model.addSketch( mypart, model.defaultPlane("XOY") )
+mybase = model.addSketch(mypart, model.defaultPlane("XOY"))
 
-p1 = geom.Pnt2d( 0, 0 )
-p2 = geom.Pnt2d( 0, 1 )
-p3 = geom.Pnt2d( 1, 1 )
-p4 = geom.Pnt2d( 1, 0 )
+p1 = geom.Pnt2d(0, 0)
+p2 = geom.Pnt2d(0, 1)
+p3 = geom.Pnt2d(1, 1)
+p4 = geom.Pnt2d(1, 0)
 
 line = mybase.addPolygon(p1, p2, p3, p4)
 
-mybase.setParallel( line[0].result(), line[2].result() )
-mybase.setParallel( line[1].result(), line[3].result() )
-mybase.setPerpendicular( line[0].result(), line[3].result() )
+mybase.setParallel(line[0].result(), line[2].result())
+mybase.setParallel(line[1].result(), line[3].result())
+mybase.setPerpendicular(line[0].result(), line[3].result())
 
-mywidth  = mybase.setLength( line[0].result(), 50 )
-mylength = mybase.setDistance( line[0].startPointData(),line[2].result(), 50 )
+mywidth = mybase.setLength(line[0].result(), 50)
+mylength = mybase.setDistance(line[0].startPointData(), line[2].result(), 50)
 
 
 # Creating the extrusion
 
-mybox = model.addExtrusion( mypart, mybase.selectFace(), 50 )
+mybox = model.addExtrusion(mypart, mybase.selectFace(), 50)
 
 
 # Creating a cylinder on a face of the box
 
-thisface = "Extrusion_1/LateralFace_2"
-thisxmin = "Extrusion_1/LateralFace_3|Extrusion_1/LateralFace_2"
-thiszmax = "Extrusion_1/LateralFace_2|Extrusion_1/TopFace_1"
+thisface = "Extrusion_1_1/LateralFace_2"
+thisxmin = "Extrusion_1_1/LateralFace_3|Extrusion_1_1/LateralFace_2"
+thiszmax = "Extrusion_1_1/LateralFace_2|Extrusion_1_1/ToFace_1"
 
-mystand = model.addSketch( mypart, thisface )
-circle  = mystand.addCircle( 0, 25, 5)
-mystand.setDistance( circle.centerData(), thisxmin, 10 )
-mystand.setDistance( circle.centerData(), thiszmax, 10 )
+mystand = model.addSketch(mypart, thisface)
+circle = mystand.addCircle(0, 25, 5)
+mystand.setDistance(circle.centerData(), thisxmin, 10)
+mystand.setDistance(circle.centerData(), thiszmax, 10)
 
-myboss = model.addExtrusion( mypart, mystand.selectFace(), -5 )
+myboss = model.addExtrusion(mypart, mystand.selectFace(), -5)
 
 
 # Subtracting the cylinder to the box
 
-model.addSubtraction( mypart, mybox.result(), myboss.result() )
+model.addSubtraction(mypart, mybox.result(), myboss.result())
 model.end()
 
 
 # Editing the box
 
 model.begin()
-mybase.setValue( mylength, 100 )
-mybox.setSize( 20 )
+mybase.setValue(mylength, 100)
+mybox.setSize(20)
 model.end()
