e6b1f42e2dd1a1eee8be163e83589a32ee308240
Renaud NEDELEC <renaud.nedelec@opencascade.com>
2015-11-02 13:13:17 +0100

    [PythonAPI / sketcher] Update of setRadius test with radius modification

--- src/PythonAPI/Test/TestSketcherSetRadius.py
+++ src/PythonAPI/Test/TestSketcherSetRadius.py
@@ -3,11 +3,19 @@ import model
 from TestSketcher import SketcherTestCase
 
 class SketcherSetRadius(SketcherTestCase):   
-    def runTest(self):
+    def test_set_radius(self):
         circle = self.sketch.addCircle(0, 10, 20)
         self.sketch.setRadius(circle.result(), 30)
         model.do()
         self.assertEqual(circle.radiusData().value(), 30.0)
         
+    def test_modify_radius(self):
+        circle = self.sketch.addCircle(0, 10, 20)
+        radius = self.sketch.setRadius(circle.result(), 30)
+        model.do()
+        self.sketch.setValue(radius, 20)
+        model.do()
+        self.assertEqual(circle.radiusData().value(), 20.0)
+        
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
