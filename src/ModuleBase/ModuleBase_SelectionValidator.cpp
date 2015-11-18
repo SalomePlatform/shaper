5a5b3f0d1a484c6bec6cc460c95bda2b3e88f1c5
Vitaly SMETANNIKOV <vsv@opencascade.com>
2015-11-18 12:29:04 +0300

    Missed source

---
+++ src/ModuleBase/ModuleBase_SelectionValidator.cpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2014-20xx CEA/DEN, EDF R&D
+
+// File:        ModuleBase_SelectionValidator.cpp
+// Created:     18 November 2015
+// Author:      Vitaly SMETANNIKOV
+
+
+#include <ModuleBase_SelectionValidator.h>
+
+
+bool ModuleBase_SelectionValidator::isValid(const ModuleBase_ISelection* theSelection, ModuleBase_Operation* theOperation) const
+{
+  return true;
+}
