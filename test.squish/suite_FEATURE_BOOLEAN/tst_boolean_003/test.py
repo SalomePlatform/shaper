971c6cb7a5f938498fd4ba8d62481b7ce054059d
spo <sergey.pokhodenko@opencascade.com>
2015-12-03 13:26:40 +0300

    Fix suite_FEATURE_BOOLEAN & suite_FEATURE_CONSTRUCTION

--- test.squish/suite_FEATURE_BOOLEAN/tst_boolean_003/test.py
+++ test.squish/suite_FEATURE_BOOLEAN/tst_boolean_003/test.py
@@ -1,41 +1,41 @@
 def main():
-    #[project] NewGEOM
-    #[Scenario] Boolean_003
-    #[Topic] 'Boolean common' functionality
-    #[Tested functionality] 
-    #[Summary description]
-    #[Expected results]
-    #[General comments]
-    
+    # [project] NewGEOM
+    # [Scenario] Boolean_003
+    # [Topic] 'Boolean common' functionality
+    # [Tested functionality]
+    # [Summary description]
+    # [Expected results]
+    # [General comments]
+
     source(findFile("scripts", "common.py"))
-    
-    #[section] Application start
-    #[step] Launch SALOME
+
+    # [section] Application start
+    # [step] Launch SALOME
     startApplication("salome_run.sh")
 
     set_defaults()
-    
-    #[step] Open 'for_extrusion_001.hdf'
+
+    # [step] Open 'for_extrusion_001.hdf'
     open(DATA_PATH + "/for_boolean_001.hdf")
-    
-    #[step] Activate NewGeom
+
+    # [step] Activate NewGeom
     clickButton(waitForObject(":SALOME*.NewGeom_QToolButton"))
-    
-    #[step] Activate Part_1
+
+    # [step] Activate Part_1
     waitForObjectItem(":Object browser_XGUI_DataTree", "Part\\_1 (Not loaded)")
     clickItem(":Object browser_XGUI_DataTree", "Part\\_1 (Not loaded)", 48, 10, 0, Qt.LeftButton)
     openItemContextMenu(waitForObject(":Object browser_XGUI_DataTree"), "Part\\_1 (Not loaded)", 48, 10, 0)
     activateItem(waitForObjectItem(":_QMenu", "Activate"))
-    
-    #[step] Fit all
+
+    # [step] Fit all
     fit_all()
     test.vp("INIT")
 
-    #[step] Implement boolean common for existing objects
+    # [step] Implement boolean common for existing objects
     boolean_common((139, 138), (420, 195))
 
-    #[check] Check that operation has been executed successfully
+    # [check] Check that operation has been executed successfully
     test.vp("COMMON")
-    
+
     # [step] Close application without saving
     close_application()
