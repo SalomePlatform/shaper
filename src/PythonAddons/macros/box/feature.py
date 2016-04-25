b02c1a358b7e36a7caf8ec3d20c02c1f58b40733
nds <nds@opencascade.com>
2016-04-25 11:58:46 +0300

    Issue #1440 Crash when edit box with parameter: using width/length parameter for sketch

--- src/PythonAddons/macros/box/feature.py
+++ src/PythonAddons/macros/box/feature.py
@@ -90,9 +90,16 @@ class BoxFeature(model.Feature):
         length = self.getRealInput(self.LENGTH_ID())
         height = self.getRealInput(self.HEIGHT_ID())
 
+        width_text = self.getTextInput(self.WIDTH_ID())
+        length_text = self.getTextInput(self.LENGTH_ID())
+        height_text = self.getTextInput(self.HEIGHT_ID())
+
         # Editing the box
         self.base.setValue(self.width, width)
+        self.base.setText(self.width, width_text)
+
         self.base.setValue(self.length, length)
+        self.base.setText(self.length, length_text)
 
         self.box.setSize(height)
 
