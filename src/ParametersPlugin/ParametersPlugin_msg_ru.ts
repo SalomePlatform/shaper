65535659031c94c6bc6a76e0b0e1e0ad22113c89
vsv <vitaly.smetannikov@opencascade.com>
2016-05-27 11:27:24 +0300

    Translation of GUI in plug-in

---
+++ src/ParametersPlugin/ParametersPlugin_msg_ru.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>ParametersPlugin_WidgetParamsMgr</name>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="165"/>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="165"/>
+        <source>Equation</source>
+        <translation>Выражение</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="165"/>
+        <source>Result</source>
+        <translation>Результат</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="165"/>
+        <source>Comment</source>
+        <translation>Коментарий</translation>
+    </message>
+    <message>
+        <source>Parameters</source>
+        <translation type="obsolete">Параметры</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="194"/>
+        <source>Features</source>
+        <translation>Объекты</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="213"/>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="217"/>
+        <source>Insert</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="221"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="258"/>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="698"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="420"/>
+        <source>Name %1 already exists.</source>
+        <translation>Имя %1 уже существует.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="ParametersPlugin_WidgetParamsMgr.cpp" line="188"/>
+        <source>Parameters</source>
+        <translation type="unfinished">Параметры</translation>
+    </message>
+</context>
+</TS>
---
+++ src/XGUI/XGUI_msg_fr.ts
@@ -0,0 +1,367 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="XGUI_Tools.cpp" line="119"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Tools.cpp" line="120"/>
+        <source>Selected objects can be used in Part documents which are not loaded: %1. Whould you like to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Tools.cpp" line="137"/>
+        <source>Selected parameter can not be renamed to: %1.  There is a parameter with the same name. Its value is: %2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="112"/>
+        <source>Move to the end</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_ActionsMgr</name>
+    <message>
+        <location filename="XGUI_ActionsMgr.cpp" line="176"/>
+        <source>Shortcut %1 is already defined. Ignore.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ActionsMgr.cpp" line="254"/>
+        <source>See preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_ContextMenuMgr</name>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="67"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="74"/>
+        <source>Rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="83"/>
+        <source>Clean history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="86"/>
+        <source>Color...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="89"/>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="92"/>
+        <source>Show only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="96"/>
+        <source>Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="100"/>
+        <source>Hide all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="104"/>
+        <source>Shading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="107"/>
+        <source>Wireframe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="117"/>
+        <source>Vertices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="123"/>
+        <source>Edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="129"/>
+        <source>Faces</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="135"/>
+        <source>Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="144"/>
+        <source>Select results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="148"/>
+        <source>Select parent feature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="561"/>
+        <source>Selection mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_ContextMenuMgr.cpp" line="610"/>
+        <source>Windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_DataTree</name>
+    <message>
+        <location filename="XGUI_ObjectsBrowser.cpp" line="161"/>
+        <source>History change</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_ErrorDialog</name>
+    <message>
+        <location filename="XGUI_ErrorDialog.cpp" line="23"/>
+        <source>Application errors</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_ObjectsBrowser</name>
+    <message>
+        <location filename="XGUI_ObjectsBrowser.cpp" line="343"/>
+        <source>Part set</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_OperationMgr</name>
+    <message>
+        <location filename="XGUI_OperationMgr.cpp" line="221"/>
+        <location filename="XGUI_OperationMgr.cpp" line="299"/>
+        <source>Abort operation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_OperationMgr.cpp" line="222"/>
+        <source>All active operations will be aborted.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_OperationMgr.cpp" line="297"/>
+        <source>%1 operation will be aborted.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_PropertyPanel</name>
+    <message>
+        <location filename="XGUI_PropertyPanel.cpp" line="49"/>
+        <location filename="XGUI_PropertyPanel.cpp" line="130"/>
+        <source>Property Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XGUI_Workshop</name>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="290"/>
+        <location filename="XGUI_Workshop.cpp" line="344"/>
+        <source>Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="291"/>
+        <location filename="XGUI_Workshop.cpp" line="344"/>
+        <source>Undo last command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="294"/>
+        <source>INF_DESK_TOOLBAR_STANDARD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="300"/>
+        <location filename="XGUI_Workshop.cpp" line="353"/>
+        <source>Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="300"/>
+        <location filename="XGUI_Workshop.cpp" line="353"/>
+        <source>Redo last command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="317"/>
+        <source>Export native...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="317"/>
+        <source>Export the current document into a native file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="322"/>
+        <source>Import native...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="322"/>
+        <source>Import native file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="334"/>
+        <location filename="XGUI_Workshop.cpp" line="823"/>
+        <source>Save</source>
+        <extracomment>Title of the dialog which asks user if he wants to save study in existing non-empty folder</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="334"/>
+        <source>Save the document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="339"/>
+        <source>Save as...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="339"/>
+        <source>Save the document into a file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="367"/>
+        <source>Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="367"/>
+        <source>Open a new document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="371"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="371"/>
+        <source>Edit preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="375"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="375"/>
+        <source>Exit application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="685"/>
+        <location filename="XGUI_Workshop.cpp" line="741"/>
+        <source>Save current file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="686"/>
+        <source>The document is modified, save before opening another?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="697"/>
+        <source>Select directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="702"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="702"/>
+        <source>Unable to open the file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="741"/>
+        <source>The document is modified, save before exit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="807"/>
+        <source>Select directory to save files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="809"/>
+        <source>Directories (*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="824"/>
+        <source>The directory already contains some files, save anyway?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="957"/>
+        <source>Information about module &quot;%1&quot; doesn&apos;t exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="1088"/>
+        <source>Object browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="1420"/>
+        <source>Unused features are the following: %1.
+These features will be deleted.
+Would you like to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="1458"/>
+        <source>All features are relevant, there is nothing to be deleted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="1958"/>
+        <source>Find results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="XGUI_Workshop.cpp" line="1959"/>
+        <source>Results not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
