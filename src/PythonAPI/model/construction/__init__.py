ec7b76c46167e93cff3dc39849253e04ff091e5f
dbv <dbv@opencascade.com>
2016-06-15 14:50:06 +0300

    Fixed addAxis import

--- src/PythonAPI/model/construction/__init__.py
+++ src/PythonAPI/model/construction/__init__.py
@@ -1,6 +1,6 @@
 """Package for Construction plugin for the Parametric Geometry API of the Modeler.
 """
 
-from axis import addAxis
+from ConstructionAPI import addAxis
 from ConstructionAPI import addPlane
 from ConstructionAPI import addPoint
