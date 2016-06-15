2cbad7a9c6838274f90cf99cb75ffa4910277533
spo <sergey.pokhodenko@opencascade.com>
2016-06-15 17:39:51 +0300

    Fix tests Radius and Tangemt. Radios does not work.

--- src/PythonAPI/Test/TestSketcherSetRadius.py
+++ src/PythonAPI/Test/TestSketcherSetRadius.py
@@ -5,17 +5,17 @@ from TestSketcher import SketcherTestCase
 class SketcherSetRadius(SketcherTestCase):
     def test_set_radius(self):
         circle = self.sketch.addCircle(0, 10, 20)
-        self.sketch.setRadius(circle.result(), 30)
+        self.sketch.setRadius(circle, 30)
         model.do()
-        self.assertEqual(circle.radius(), 30.0)
+        self.assertEqual(circle.radius().value(), 30.0)
 
     def test_modify_radius(self):
         circle = self.sketch.addCircle(0, 10, 20)
-        radius_constraint = self.sketch.setRadius(circle.result(), 30)
+        radius_constraint = self.sketch.setRadius(circle, 30)
         model.do()
-        self.sketch.setValue(radius_constraint, 20)
+        self.sketch.setValue(radius_constraint, 25)
         model.do()
-        self.assertEqual(circle.radius(), 20.0)
+        self.assertEqual(circle.radius().value(), 25.0)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
