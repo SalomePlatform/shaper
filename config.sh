b0c46533983829a5795b42ec80296f029daf54e1
spo <sergey.pokhodenko@opencascade.com>
2016-06-23 12:41:49 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -25,7 +25,8 @@ if [ -f PLATFORM ]; then
   RELEASE="$(lsb_release -rs)"
   if [ "${PLATFORM}" = 'CentOS' ]; then PLATFORM=-centos
   elif [ "${PLATFORM}" = 'Debian' ]; then
-    if [ "${RELEASE}" = '8.0' ]; then PLATFORM=-d8
+    if [ "${RELEASE}" =~ ^8 ]; then PLATFORM=-d8
+    elif [ "${RELEASE}" =~ ^7 ]; then PLATFORM=-d7
     else PLATFORM=-d6
     fi
   fi
@@ -48,6 +49,7 @@ include_products() {
   elif [[ ${salome_version} = '7.8.0' ]]; then
     if   [[ "${platform}" = 'CentOS' ]]; then export PRODUCTS_ROOT=/dn23/NEWGEOM/common/PRODUCTS/CentOS.6.3.64/7.8.0
     elif [[ "${platform}" = 'Debian' && ${release} =~ ^6 ]]; then export PRODUCTS_ROOT=/dn23/NEWGEOM/common/PRODUCTS/DEBIAN.6.0.64/7.8.0
+    elif [[ "${platform}" = 'Debian' && ${release} =~ ^7 ]]; then export PRODUCTS_ROOT=/dn23/NEWGEOM/common/PRODUCTS/DEBIAN.7.0.64/7.8.0
     fi
   else
     echo "Could not find PRODUCTS for the platform ${platform} ${release}"
@@ -67,6 +69,7 @@ include_suit() {
   elif [[ ${salome_version} = '7.8.0' ]]; then
     if   [[ "${platform}" = 'CentOS' ]]; then export SUIT_DIR=/dn46/SALOME/series7x/V7_8_0/CentOS63-64/GUI
     elif [[ "${platform}" = 'Debian' && ${release} =~ ^6 ]]; then export SUIT_DIR=/dn46/SALOME/series7x/V7_8_0/Debian60-64/GUI
+    elif [[ "${platform}" = 'Debian' && ${release} =~ ^7 ]]; then export SUIT_DIR=/dn46/SALOME/series7x/V7_8_0/Debian70-64/GUI
     fi
   else
     echo "Could not find PRODUCTS for the platform ${platform} ${release}"
@@ -95,6 +98,7 @@ include_salome() {
   elif [[ ${salome_version} = '7.8.0' ]]; then
     if   [[ "${platform}" = 'CentOS' ]]; then export SALOME_ROOT=/dn23/NEWGEOM/common/SALOME-7.8.0_CentOS63-64
     elif [[ "${platform}" = 'Debian' && ${release} =~ ^6 ]]; then export SALOME_ROOT=/dn23/NEWGEOM/common/SALOME-7.8.0_Debian60-64
+    elif [[ "${platform}" = 'Debian' && ${release} =~ ^7 ]]; then export SALOME_ROOT=/dn23/NEWGEOM/common/SALOME-7.8.0_Debian70-64
     fi
   else 
     echo "Could not find SALOME for the platform ${platform} ${release}"
