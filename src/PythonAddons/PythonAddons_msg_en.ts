cad9fa921c4106c03e29b9ea61f281e5d417b688
nds <nds@opencascade.com>
2016-07-04 10:34:16 +0300

    #1603 Error on the launch SHAPER 2.4.0

--- src/PythonAddons/PythonAddons_msg_en.ts
+++ src/PythonAddons/PythonAddons_msg_en.ts
@@ -8,66 +8,65 @@
     <translation>Height of the box is not defined.</translation>
   </message>
 
-    <message>
-      <source>height - GeomValidators_Positive: Double is not initialized.</source>
-      <translation>Height of the box is not defined.</translation>
-    </message>
-    <message>
-      <source>height - GeomValidators_Positive: Double is not positive.</source>
-      <translation>Height of the box is too small.</translation>
-    </message>
-    <message>
-      <source>height - GeomValidators_Positive: Integer is not initialized.</source>
-      <translation>Height of the box is not defined.</translation>
-    </message>
-    <message>
-      <source>height - GeomValidators_Positive: Integer is not positive.</source>
-      <translation>Height of the box is too small.</translation>
-    </message>
-    <message>
-      <source>Model_FeatureValidator: Attribute "width" is not initialized.</source>
-      <translation>Set the box width</translation>
-    </message>
-    <message>
-      <source>Model_FeatureValidator: Attribute "length" is not initialized.</source>
-      <translation>Set the box length</translation>
-    </message>
-    <message>
-      <source>Model_FeatureValidator: Attribute "height" is not initialized.</source>
-      <translation>Set the box height</translation>
-    </message>
-    <message>
-      <source>width - GeomValidators_Positive: Double is not initialized.</source>
-      <translation>Set the box width</translation>
-    </message>
-    <message>
-      <source>width - GeomValidators_Positive: Double is not positive.</source>
-      <translation>The box width must be positive</translation>
-    </message>
-    <message>
-      <source>width - GeomValidators_Positive: Integer is not initialized.</source>
-      <translation></translation>
-    </message>
-    <message>
-      <source>width - GeomValidators_Positive: Integer is not positive.</source>
-      <translation></translation>
-    </message>
-    <message>
-      <source>length - GeomValidators_Positive: Double is not initialized.</source>
-      <translation>Set the box length</translation>
-    </message>
-    <message>
-      <source>length - GeomValidators_Positive: Double is not positive.</source>
-      <translation>The box width must be positive</translation>
-    </message>
-    <message>
-      <source>length - GeomValidators_Positive: Integer is not initialized.</source>
-      <translation></translation>
-    </message>
-    <message>
-      <source>length - GeomValidators_Positive: Integer is not positive.</source>
-      <translation></translation>
-    </message>
-    </message>  
+  <message>
+    <source>height - GeomValidators_Positive: Double is not initialized.</source>
+    <translation>Height of the box is not defined.</translation>
+  </message>
+  <message>
+    <source>height - GeomValidators_Positive: Double is not positive.</source>
+    <translation>Height of the box is too small.</translation>
+  </message>
+  <message>
+    <source>height - GeomValidators_Positive: Integer is not initialized.</source>
+    <translation>Height of the box is not defined.</translation>
+  </message>
+  <message>
+    <source>height - GeomValidators_Positive: Integer is not positive.</source>
+    <translation>Height of the box is too small.</translation>
+  </message>
+  <message>
+    <source>Model_FeatureValidator: Attribute "width" is not initialized.</source>
+    <translation>Set the box width</translation>
+  </message>
+  <message>
+    <source>Model_FeatureValidator: Attribute "length" is not initialized.</source>
+    <translation>Set the box length</translation>
+  </message>
+  <message>
+    <source>Model_FeatureValidator: Attribute "height" is not initialized.</source>
+    <translation>Set the box height</translation>
+  </message>
+  <message>
+    <source>width - GeomValidators_Positive: Double is not initialized.</source>
+    <translation>Set the box width</translation>
+  </message>
+  <message>
+    <source>width - GeomValidators_Positive: Double is not positive.</source>
+    <translation>The box width must be positive</translation>
+  </message>
+  <message>
+    <source>width - GeomValidators_Positive: Integer is not initialized.</source>
+    <translation></translation>
+  </message>
+  <message>
+    <source>width - GeomValidators_Positive: Integer is not positive.</source>
+    <translation></translation>
+  </message>
+  <message>
+    <source>length - GeomValidators_Positive: Double is not initialized.</source>
+    <translation>Set the box length</translation>
+  </message>
+  <message>
+    <source>length - GeomValidators_Positive: Double is not positive.</source>
+    <translation>The box width must be positive</translation>
+  </message>
+  <message>
+    <source>length - GeomValidators_Positive: Integer is not initialized.</source>
+    <translation></translation>
+  </message>
+  <message>
+    <source>length - GeomValidators_Positive: Integer is not positive.</source>
+    <translation></translation>
+  </message>
 </context>
 </TS>
