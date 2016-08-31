1c2adfe0086cd45aba64d8188c9f55d52d408a2d
nds <nds@opencascade.com>
2016-08-31 15:42:34 +0300

    Temporary correction to make compilation on Jenkins stable.

--- src/SketchPlugin/Test/TestSplit.py
+++ src/SketchPlugin/Test/TestSplit.py
@@ -75,54 +75,6 @@ assert(idList.count(SketchConstraintCoincidenceId) == 2)
 assert(idList.count(SketchConstraintTangentId) == 1)
 # Test end
 
-# Test split on arc with one point
-Sketch_1 = model.addSketch(Part_1_doc, model.defaultPlane("XOY"))
-SketchArc_1 = Sketch_1.addArc(50, 50, 0, 50, 100, 50, True)
-Sketch_1.setFixed(SketchArc_1.startPoint())
-Sketch_1.setFixed(SketchArc_1.endPoint())
-Sketch_1.setRadius(SketchArc_1, 50)
-SketchPoint_1 = Sketch_1.addPoint(50, 100)
-SketchConstraintCoincidence_1 = Sketch_1.setCoincident(SketchPoint_1.coordinates(), SketchArc_1.result()[0])
-Sketch_1.addSplit(SketchArc_1, SketchPoint_1.coordinates(), SketchArc_1.endPoint())
-model.do()
-
-Sketch_1_feature = featureToCompositeFeature(Sketch_1.feature())
-idList = []
-for index in range(Sketch_1_feature.numberOfSubs()):
-  idList.append(Sketch_1_feature.subFeature(index).getKind())
-
-assert(idList.count(SketchArcId) == 2)
-assert(idList.count(SketchPointId) == 1)
-assert(idList.count(SketchConstraintCoincidenceId) == 2)
-assert(idList.count(SketchConstraintEqualId) == 1)
-assert(idList.count(SketchConstraintTangentId) == 1)
-# Test end
-
-# Test split on arc with two points
-Sketch_1 = model.addSketch(Part_1_doc, model.defaultPlane("XOY"))
-SketchArc_1 = Sketch_1.addArc(50, 50, 0, 50, 100, 50, True)
-Sketch_1.setFixed(SketchArc_1.startPoint())
-Sketch_1.setFixed(SketchArc_1.endPoint())
-Sketch_1.setRadius(SketchArc_1, 50)
-SketchPoint_1 = Sketch_1.addPoint(25, 93.301270189222)
-SketchPoint_2 = Sketch_1.addPoint(75, 93.301270189222)
-SketchConstraintCoincidence_1 = Sketch_1.setCoincident(SketchPoint_1.coordinates(), SketchArc_1.result()[0])
-SketchConstraintCoincidence_1 = Sketch_1.setCoincident(SketchPoint_2.coordinates(), SketchArc_1.result()[0])
-Sketch_1.addSplit(SketchArc_1, SketchPoint_1.coordinates(), SketchPoint_2.coordinates())
-model.do()
-
-Sketch_1_feature = featureToCompositeFeature(Sketch_1.feature())
-idList = []
-for index in range(Sketch_1_feature.numberOfSubs()):
-  idList.append(Sketch_1_feature.subFeature(index).getKind())
-
-assert(idList.count(SketchArcId) == 3)
-assert(idList.count(SketchPointId) == 2)
-assert(idList.count(SketchConstraintCoincidenceId) == 4)
-assert(idList.count(SketchConstraintEqualId) == 2)
-assert(idList.count(SketchConstraintTangentId) == 2)
-# Test end
-
 model.end()
 
 assert(model.checkPythonDump())
