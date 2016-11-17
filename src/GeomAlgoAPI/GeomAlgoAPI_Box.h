7e5b8f1a137a13ad1eb2d7996274b85284a8e0d7
Clarisse Genrault <clarisse.genrault@cea.fr>
2016-11-17 09:55:45 +0100

    Deleting whitespace of endof lines.

--- src/GeomAlgoAPI/GeomAlgoAPI_Box.h
+++ src/GeomAlgoAPI/GeomAlgoAPI_Box.h
@@ -22,27 +22,27 @@ class GeomAlgoAPI_Box : public GeomAlgoAPI_MakeShape
     BOX_DIM,   ///< Box with dimensions
     BOX_POINTS,  ///< Box with points
   };
-  
+
   GEOMALGOAPI_EXPORT GeomAlgoAPI_Box();
-  
+
   /// Creates a box using the dimensions.
   /// \param theDx The dimension on X
   /// \param theDy The dimension on Y
   /// \param theDz The dimension on Z
   GEOMALGOAPI_EXPORT GeomAlgoAPI_Box(const double theDx, const double theDy, const double theDz);
-  
+
   /// Creates a box using the two points that defined a diagonal.
   /// \param theFirstPoint One extermity of the diagonal
   /// \param theSecondPoint The other extremity of the diagonal
   GEOMALGOAPI_EXPORT GeomAlgoAPI_Box(std::shared_ptr<GeomAPI_Pnt> theFirstPoint,
                                      std::shared_ptr<GeomAPI_Pnt> theSecondPoint);
-  
+
   /// Checks if data for the box construction is OK.
   GEOMALGOAPI_EXPORT bool check();
-  
+
   /// Builds the box.
   GEOMALGOAPI_EXPORT void build();
-  
+
   /// Prepare the naming (redifined because it is specific for a box).
   GEOMALGOAPI_EXPORT void prepareNamingFaces();
 
@@ -51,7 +51,7 @@ class GeomAlgoAPI_Box : public GeomAlgoAPI_MakeShape
   void buildWithDimensions();
   /// Builds the box with two points
   void buildWithPoints();
-  
+
   double myDx; /// Dimension on X to create a box.
   double myDy; /// Dimension on Y to create a box.
   double myDz; /// Dimension Z to create a box.
