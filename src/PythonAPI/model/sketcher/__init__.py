2ab48627ddcce7642cd320525e87776421b95387
azv <azv@opencascade.com>
2016-11-23 16:29:12 +0300

    Workaround for Debian 6.0 (issues #1868 and #1878) Loading of SketchPlugin should be before loading of SketchAPI module

--- src/PythonAPI/model/sketcher/__init__.py
+++ src/PythonAPI/model/sketcher/__init__.py
@@ -1,5 +1,20 @@
 """Package for Sketch plugin for the Parametric Geometry API of the Modeler.
 """
 
+# DEBIAN 6.0 WORKAROUND START
+"""
+  Following code is used to support Debian 6.0.
+  It is kept, because SketchPlugin should be loaded before SketchAPI is first used.
+  Otherwise, the runtime error will be generated while some static inline methods are broken.
+""" 
+from ModelAPI import *
+aSession = ModelAPI_Session.get()
+aDocument = aSession.moduleDocument()
+aSession.startOperation()
+aDocument.addFeature("Sketch")
+aSession.finishOperation()
+aSession.abortOperation()
+# DEBIAN 6.0 WORKAROUND FINISH
+
 from SketchAPI import addSketch
 from tools import addPolyline, addPolygon
