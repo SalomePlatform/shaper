881cf7420dccf3c43837390efa9cded7b24836a2
azv <azv@opencascade.com>
2016-11-24 08:28:48 +0300

    Workaround for Debian 6.0 (issues #1868 and #1878) Correction.

--- src/PythonAPI/model/sketcher/__init__.py
+++ src/PythonAPI/model/sketcher/__init__.py
@@ -7,13 +7,9 @@
   It is kept, because SketchPlugin should be loaded before SketchAPI is first used.
   Otherwise, the runtime error will be generated while some static inline methods are broken.
 """ 
-from ModelAPI import *
+from ModelAPI import ModelAPI_Session
 aSession = ModelAPI_Session.get()
-aDocument = aSession.moduleDocument()
-aSession.startOperation()
-aDocument.addFeature("Sketch")
-aSession.finishOperation()
-aSession.abortOperation()
+aSession.forceLoadPlugin("SketchPlugin")
 # DEBIAN 6.0 WORKAROUND FINISH
 
 from SketchAPI import addSketch
