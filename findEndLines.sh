7e2d6156ee09a4b00fb6e1334d078f602273cf60
azv <artem.zhidkov@opencascade.com>
2016-11-29 14:36:09 +0300

    Add cpplint to products for checking Jenkins build procedure

--- findEndLines.sh
+++ findEndLines.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source env.sh
+
 find . \( -name "*.h" -o -name "*.cpp" \) -exec cpplint.py --filter=-,+whitespace/end_of_line  --verbose=0 --counting=detailed {} \+ 2>&1 | grep -v Done | tee ./tofix
 
-python correction_auto_2.py --cpplint_output_file ./tofix
+#python correction_auto_2.py --cpplint_output_file ./tofix
 
 rm ./tofix
