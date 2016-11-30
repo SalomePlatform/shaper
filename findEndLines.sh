7b3c4a3a5474d302a55082bb7f896745a0d9e8bf
azv <artem.zhidkov@opencascade.com>
2016-11-30 08:40:10 +0300

    Add key for automatic fixing of line endings

--- findEndLines.sh
+++ findEndLines.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+FIX_LINE_ENDINGS=0
+while [[ $# > 0 ]]; do
+  key="$1"
+  
+  case $key in
+    fix)
+      FIX_LINE_ENDINGS=1
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
 source env.sh
 
 find . \( -name "*.h" -o -name "*.cpp" \) -exec cpplint.py --filter=-,+whitespace/end_of_line  --verbose=0 --counting=detailed {} \+ 2>&1 | grep -v Done | tee ./tofix
 
-#python correction_auto_2.py --cpplint_output_file ./tofix
+WRONG=0
+if [ -s ./tofix ]; then
+  if [[ ${FIX_LINE_ENDINGS} == 1 ]]; then
+    python correction_auto_2.py --cpplint_output_file ./tofix
+    echo "NOTE: Line endings are fixed"
+  else
+    WRONG=1
+  fi
+fi
 
 rm ./tofix
+exit $WRONG
