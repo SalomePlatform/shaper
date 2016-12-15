65e69dfe301aabb89af5999edfba30e9e20948e9
vsv <vitaly.smetannikov@opencascade.com>
2016-12-15 18:01:32 +0300

    Fix code formatting

--- src/SHAPERGUI/SHAPERGUI.cpp
+++ src/SHAPERGUI/SHAPERGUI.cpp
@@ -154,7 +154,7 @@ void SHAPERGUI::viewManagers(QStringList& theList) const
 //  // proxy viewer. If study is opened, CAM application calls this method before the open()
 //  // of data model
 //  // the SHAPER data model is specific and during open(load) redisplay signals are flushed, so
-//  // we need to connect to the viewer before it. Here, 
+//  // we need to connect to the viewer before it. Here,
 //  // it seems the most appropriate place for this
 //  // according to SALOME architecture.
 //  if (!mySelector) {
