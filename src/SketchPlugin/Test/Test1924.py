c3a819e00709b8157dd82015c8f46d3668b86b15
azv <azv@opencascade.com>
2017-01-09 12:45:05 +0300

    Fix problem with dumping names of entities copied by Multi-Rotation/Translation (issue #1924)

---
+++ src/SketchPlugin/Test/Test1924.py
@@ -0,0 +1,28 @@
+"""
+    Test1924.py
+    Test case for issue #1924 "Wrong naming of multiple-rotated sketch edges on python dump"
+"""
+
+from salome.shaper import model
+
+model.begin()
+partSet = model.moduleDocument()
+Part_1 = model.addPart(partSet)
+Part_1_doc = Part_1.document()
+
+Sketch_1 = model.addSketch(Part_1_doc, model.defaultPlane("XOY"))
+SketchLine_1 = Sketch_1.addLine(70, 120, 50, 25)
+SketchLine_2 = Sketch_1.addLine(50, 25, 170, 50)
+SketchPoint_1 = Sketch_1.addPoint(model.selection("VERTEX", "PartSet/Origin"))
+SketchMultiRotation_1_objects = [SketchLine_1.result(), SketchLine_2.result()]
+# names of rotated objects: "SketchLine_3", "SketchLine_4"
+SketchMultiRotation_1 = Sketch_1.addRotation(SketchMultiRotation_1_objects, SketchPoint_1.coordinates(), 90, 2)
+# line named "SketchLine_5"
+SketchLine_5 = Sketch_1.addLine(0, 100, 100, 100)
+# change number of rotated objects.
+# names of rotated objects: "SketchLine_3", "SketchLine_4", "SketchLine_6", "SketchLine_7"
+SketchMultiRotation_1.numberOfObjects().setValue(3)
+
+model.end()
+
+assert(model.checkPythonDump())
