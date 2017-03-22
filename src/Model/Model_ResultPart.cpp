f31c6ecdf45d8913ae09f4bd577e33c72ff0ca8c
mpv <mpv@opencascade.com>
2017-03-22 15:29:06 +0300

    Fix fir the issue #2035 : crash on remove of depended Parts

--- src/Model/Model_ResultPart.cpp
+++ src/Model/Model_ResultPart.cpp
@@ -48,7 +48,7 @@ void Model_ResultPart::initAttributes()
 
 std::shared_ptr<ModelAPI_Document> Model_ResultPart::partDoc()
 {
-  if (myTrsf.get()) {
+  if (myTrsf.get() && baseRef().get()) { // the second condition is to to #2035
     return baseRef()->partDoc();
   }
   DocumentPtr aRes = data()->document(DOC_REF())->value();
@@ -99,7 +99,7 @@ void Model_ResultPart::activate()
 
 std::shared_ptr<ModelAPI_ResultPart> Model_ResultPart::original()
 {
-  if (myTrsf.get()) {
+  if (myTrsf.get() && baseRef().get()) {  // the second condition is to to #2035
     return baseRef()->original();
   }
   return std::dynamic_pointer_cast<ModelAPI_ResultPart>(data()->owner());
