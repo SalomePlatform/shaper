5d7f750953e789398ff83a0414ef3cc736a3b4ba
azv <azv@opencascade.com>
2018-11-03 14:06:40 +0300

    Update unit tests for ModelHighAPI module

--- src/ModelHighAPI/Test/TestDeflectionDump.py
+++ src/ModelHighAPI/Test/TestDeflectionDump.py
@@ -28,7 +28,7 @@ Sketch_1 = model.addSketch(Part_1_doc, model.defaultPlane("XOY"))
 SketchCircle_1 = Sketch_1.addCircle(-325.90051457976, -18.010291595197, 258.003584159371)
 model.do()
 Sketch_1.result().setDeflection(5e-006)
-Extrusion_1 = model.addExtrusion(Part_1_doc, [model.selection("FACE", "Sketch_1/Face-SketchCircle_1_2f")], model.selection(), 10, 0)
+Extrusion_1 = model.addExtrusion(Part_1_doc, [model.selection("FACE", "Sketch_1/Face-SketchCircle_1_2r")], model.selection(), 10, 0)
 Extrusion_1.result().setDeflection(0.5)
 model.end()
 
