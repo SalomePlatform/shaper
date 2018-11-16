3b2022a46ef6d7c12d7a8574499148e371f8b825
mpv <mpv@opencascade.com>
2018-11-16 13:09:55 +0300

    Add Primitives and GDML packages (both Plugins and API) to the code coverage reports.

--- lcov_reports.sh
+++ lcov_reports.sh
@@ -9,7 +9,7 @@ lcov --capture --directory /data/mpv/build --no-external --base-directory=/data/
 # make a working copy of report
 cp -f coverage.info.noext covfile
 # remove all reports of GUI and external parts (for all the next kinds of reports)
-for MASK in '*wrap*' 'moc_*' 'XAO_*' 'SketcherPrs_*' 'GeomAlgoImpl_*' 'ModuleBase_*' 'Primitives*' '*Widget*' '*Splitter*'; do
+for MASK in '*wrap*' 'moc_*' 'XAO_*' 'SketcherPrs_*' 'GeomAlgoImpl_*' 'ModuleBase_*' '*Widget*' '*Splitter*'; do
 lcov -r covfile ${MASK} --output-file covfile_res -q
 mv -f covfile_res covfile
 done
@@ -18,12 +18,12 @@ done
 # prepare API report
 cp -f covfile covAPI
 # remove plugins
-for MASK in 'Build' 'Collection' 'Construction' 'Exchange' 'Features' 'GDML' 'Initialization' 'Parameters' 'PartSet' 'Sketch'; do
+for MASK in 'Build' 'Collection' 'Construction' 'Exchange' 'Features' 'GDML' 'Primitives' 'Initialization' 'Parameters' 'PartSet' 'Sketch'; do
 lcov -r covAPI *${MASK}Plugin* --output-file covAPI_res -q
 mv -f covAPI_res covAPI
 done
 # remove low level API
-for MASK in 'GDML' 'Geom' 'GeomAlgo' 'GeomData' 'Model'; do
+for MASK in 'Geom' 'GeomAlgo' 'GeomData' 'Model'; do
 lcov -r covAPI *${MASK}API* --output-file covAPI_res -q
 mv -f covAPI_res covAPI
 done
@@ -39,13 +39,13 @@ genhtml covAPI --output-directory lcov_htmlAPI -q
 # prepare Direct report
 cp -f covfile covDirect
 # remove plugins
-for MASK in 'Build' 'Collection' 'Construction' 'Exchange' 'Features' 'GDML' 'Initialization' 'Parameters' 'PartSet' 'Sketch'; do
+for MASK in 'Build' 'Collection' 'Construction' 'Exchange' 'Features' 'GDML' 'Primitives' 'Initialization' 'Parameters' 'PartSet' 'Sketch'; do
 str=$startmask$MASK$endmask
 lcov -r covDirect *${MASK}Plugin* --output-file covDirect_res -q
 mv -f covDirect_res covDirect
 done
 # remove low level API
-for MASK in 'GDML' 'Builder' 'Collection' 'Connector' 'Construction' 'Model' 'Exchange' 'Features' 'ModelHigh' 'Parameters' 'PartSet' 'Sketch' 'Build' 'GeomData'; do
+for MASK in 'GDML' 'Primitives' 'Builder' 'Collection' 'Connector' 'Construction' 'Model' 'Exchange' 'Features' 'ModelHigh' 'Parameters' 'PartSet' 'Sketch' 'Build' 'GeomData'; do
 lcov -r covDirect *${MASK}API* --output-file covDirect_res -q
 mv -f covDirect_res covDirect
 done
@@ -60,14 +60,8 @@ genhtml covDirect --output-directory lcov_htmlDirect -q
 
 # prepare Else report
 cp -f covfile covElse
-# remove plugins
-for MASK in 'GDML'; do
-str=$startmask$MASK$endmask
-lcov -r covElse *${MASK}Plugin* --output-file covElse_res -q
-mv -f covElse_res covElse
-done
 # remove low level API
-for MASK in 'Geom' 'GeomAlgo' 'GDML' 'Builder' 'Collection' 'Connector' 'Construction' 'Exchange' 'Features' 'ModelHigh' 'Parameters' 'PartSet' 'Sketch' 'Build'; do
+for MASK in 'Geom' 'GeomAlgo' 'GDML' 'Primitives' 'Builder' 'Collection' 'Connector' 'Construction' 'Exchange' 'Features' 'ModelHigh' 'Parameters' 'PartSet' 'Sketch' 'Build'; do
 lcov -r covElse *${MASK}API* --output-file covElse_res -q
 mv -f covElse_res covElse
 done
