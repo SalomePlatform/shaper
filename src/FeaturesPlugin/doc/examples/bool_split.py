a046d5af78f8d7b65551d3c68ca71cff33470fe2
Clarisse GENRAULT - DEN/DANS/DM2S/STMF/LGLS - 2012/10/01 <cg246364@is231796.intra.cea.fr>
2019-03-07 08:56:51 +0100

    Renaming Fill in Split : Help (change name of result)

--- src/FeaturesPlugin/doc/examples/bool_split.py
+++ src/FeaturesPlugin/doc/examples/bool_split.py
@@ -10,8 +10,8 @@ Box_1 = model.addBox(Part_1_doc, 10, 10, 10)
 Cylinder_1 = model.addCylinder(Part_1_doc,
                                model.selection("VERTEX", "PartSet/Origin"),
                                model.selection("EDGE", "PartSet/OZ"), 5, 10)
-Fill_1 = model.addSplit(Part_1_doc,
-                        [model.selection("SOLID", "Cylinder_1_1")],
-                        [model.selection("SOLID", "Box_1_1")])
+Split_1 = model.addSplit(Part_1_doc,
+                         [model.selection("SOLID", "Cylinder_1_1")],
+                         [model.selection("SOLID", "Box_1_1")])
 model.do()
 model.end()
