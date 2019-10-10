97b59dde5ee2c9da1dc1779134bf0543f4cfe805
mpv <mpv@opencascade.com>
2019-10-10 10:32:22 +0300

    Move to the latest SALOME version.

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-export SALOME_DIR=/dn46/SALOME/series9x/current-2019-10-04
+export SALOME_DIR=/dn46/SALOME/series9x/current-2019-10-10
 
 # Path to sources
 export SOURCES_DIR=$(pwd)
