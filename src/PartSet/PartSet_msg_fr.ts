dc6ab7150ec816d25d7be4959407b6622b019e88
vsv <vsv@opencascade.com>
2019-11-06 13:29:22 +0300

    Restore translations

---
+++ src/PartSet/PartSet_msg_fr.ts
@@ -0,0 +1,175 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>PartSet_MenuMgr</name>
+    <message>
+        <location filename="PartSet_MenuMgr.cpp" line="91"/>
+        <location filename="PartSet_MenuMgr.cpp" line="529"/>
+        <source>Auxiliary</source>
+        <translation>Auxiliaire</translation>
+    </message>
+    <message>
+        <location filename="PartSet_MenuMgr.cpp" line="95"/>
+        <location filename="PartSet_MenuMgr.cpp" line="100"/>
+        <source>Activate</source>
+        <translation>Activer</translation>
+    </message>
+    <message>
+        <location filename="PartSet_MenuMgr.cpp" line="104"/>
+        <source>Edit...</source>
+        <translation>Modifier...</translation>
+    </message>
+    <message>
+        <location filename="PartSet_MenuMgr.cpp" line="185"/>
+        <location filename="PartSet_MenuMgr.cpp" line="324"/>
+        <location filename="PartSet_MenuMgr.cpp" line="528"/>
+        <source>Detach</source>
+        <translation>Détacher</translation>
+    </message>
+    <message>
+        <location filename="PartSet_MenuMgr.cpp" line="317"/>
+        <source>Detach %1</source>
+        <translation>Détachez %1</translation>
+    </message>
+</context>
+<context>
+    <name>PartSet_WidgetPoint2D</name>
+    <message>
+        <location filename="PartSet_WidgetPoint2d.cpp" line="118"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetPoint2d.cpp" line="134"/>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+</context>
+<context>
+    <name>PartSet_WidgetSketchCreator</name>
+    <message>
+        <location filename="PartSet_WidgetSketchCreator.cpp" line="91"/>
+        <source>Size of the view</source>
+        <translation>Taille de la vue</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchCreator.cpp" line="546"/>
+        <source>Apply current feature</source>
+        <translation>Appliquer la fonctionnalité actuelle</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchCreator.cpp" line="547"/>
+        <source>Sketch is invalid and will be deleted.
+Error: %1</source>
+        <translation>L&apos;esquisse n&apos;est pas valide et sera supprimée.
+Erreur : %1</translation>
+    </message>
+</context>
+<context>
+    <name>PartSet_WidgetSketchLabel</name>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="106"/>
+        <source>Size of the view</source>
+        <translation>Taille de la vue</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="127"/>
+        <source>Remove external dependencies</source>
+        <translation>Supprimer les dépendances externes</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="141"/>
+        <source>Sketcher plane</source>
+        <translation>Plan du Sketcher</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="144"/>
+        <source>Reversed</source>
+        <translation>Renversé</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="148"/>
+        <source>Set plane view</source>
+        <translation>Définir la vue plane</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="155"/>
+        <source>Show geometrical constraints</source>
+        <translation>Afficher les contraintes géométriques</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="156"/>
+        <source>Show dimensional constraints</source>
+        <translation>Afficher les contraintes dimensionnelles</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="157"/>
+        <source>Show existing expressions</source>
+        <translation>Afficher les expressions existantes</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="172"/>
+        <source>Show free points</source>
+        <translation>Afficher les points libres</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="176"/>
+        <source>Automatic constraints</source>
+        <translation>Contraintes automatiques</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="177"/>
+        <source>Automatic vertical and horizontal constraints</source>
+        <translation>Automatique des contraintes verticales et horizontales</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="181"/>
+        <source>Change sketch plane</source>
+        <translation>Changer le plan d&apos;esquisse</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="753"/>
+        <source>Sketch is fully fixed (DoF = 0)</source>
+        <translation>L&apos;esquisse est entièrement fixée (DdL = 0)</translation>
+    </message>
+    <message>
+        <location filename="PartSet_WidgetSketchLabel.cpp" line="755"/>
+        <source>DoF (degrees of freedom) = </source>
+        <translation>DdL (degrés de liberté) = </translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="PartSet_TreeNodes.cpp" line="402"/>
+        <source>Parameters</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="PartSet_TreeNodes.cpp" line="404"/>
+        <source>Constructions</source>
+        <translation>Constructions</translation>
+    </message>
+    <message>
+        <location filename="PartSet_TreeNodes.cpp" line="406"/>
+        <source>Parts</source>
+        <translation>Pièces</translation>
+    </message>
+    <message>
+        <location filename="PartSet_TreeNodes.cpp" line="408"/>
+        <source>Results</source>
+        <translation>Résultats</translation>
+    </message>
+    <message>
+        <location filename="PartSet_TreeNodes.cpp" line="410"/>
+        <source>Fields</source>
+        <translation>Champs</translation>
+    </message>
+    <message>
+        <location filename="PartSet_TreeNodes.cpp" line="412"/>
+        <source>Groups</source>
+        <translation>Groupes</translation>
+    </message>
+</context>
+</TS>
