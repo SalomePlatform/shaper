87cf064d1bd7c69d307acc502bc5dff95b7c4ccd
vsv <vsv@opencascade.com>
2019-11-28 17:33:41 +0300

    Issue #3108: Send event when show expression is requested

--- src/PartSet/PartSet_SketcherMgr.cpp
+++ src/PartSet/PartSet_SketcherMgr.cpp
@@ -2001,6 +2001,7 @@ void PartSet_SketcherMgr::updateBySketchParameters(
       if (aPrevState != theState) {
         /// call all sketch features redisplay, the expression state will be corrected in customize
         /// of distance presentation
+        SketcherPrs_Tools::sendExpressionShownEvent(myIsConstraintsShown[PartSet_Tools::Expressions]);
         Events_ID anEventId = Events_Loop::loop()->eventByName(EVENT_OBJECT_TO_REDISPLAY);
         PartSet_Tools::sendSubFeaturesEvent(myCurrentSketch, anEventId);
       }
