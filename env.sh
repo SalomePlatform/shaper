8a57f645f10b4c37dda0d115cb68d801285512d9
azv <azv@opencascade.com>
2020-01-29 10:19:18 +0300

    Revert version of SALOME to 2019-10-10

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-export SALOME_DIR=/dn46/SALOME/series9x/current-2020-01-29
+export SALOME_DIR=/dn46/SALOME/series9x/current-2019-10-10
 
 # Path to sources
 export SOURCES_DIR=$(pwd)
