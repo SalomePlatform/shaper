460ad564458a984f209519d2c7e1475c501251b9
azv <azv@opencascade.com>
2020-02-03 15:54:17 +0300

    Reference data for new restricted model.

---
+++ test.hdfs/CuveN4_Tubulures.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2014-2019  CEA/DEN, EDF R&D
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
+
+if __name__ == "__main__":
+  aPartFeature = locals()["Part_1"]
+  model.testNbResults(aPartFeature, 1)
+  model.testNbSubResults(aPartFeature, [0])
+  model.testNbSubShapes(aPartFeature, GeomAPI_Shape.SOLID, [552])
+  model.testNbSubShapes(aPartFeature, GeomAPI_Shape.FACE, [4086])
+  model.testNbSubShapes(aPartFeature, GeomAPI_Shape.EDGE, [17086])
+  model.testNbSubShapes(aPartFeature, GeomAPI_Shape.VERTEX, [34172])
+  model.testResultsVolumes(aPartFeature, [55.645173358299])
