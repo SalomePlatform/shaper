e5aaf6f289d3d9739371d07f6de06f0ce86f284e
vsv <vsv@opencascade.com>
2020-03-03 11:59:00 +0300

    Update French translation

--- src/ConnectorPlugin/ConnectorPlugin_msg_fr.ts
+++ src/ConnectorPlugin/ConnectorPlugin_msg_fr.ts
@@ -27,4 +27,123 @@
       <translation>Pour la publication sur SHAPER-STUDY, une partie n'est pas activée</translation>
     </message>
   </context>
+
+  <context>
+    <name>ExportPart</name>
+    <message>
+      <source>Export part</source>
+      <translation>Partie exportation</translation>
+    </message>
+    <message>
+      <source>Export structure of the Part to file</source>
+      <translation>Structure d'exportation de la partie à classer</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ImportMacro</name>
+    <message>
+      <source>Import</source>
+      <translation>Importation</translation>
+    </message>
+    <message>
+      <source>Import a file</source>
+      <translation>Importer un fichier</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ImportPart</name>
+    <message>
+      <source>Import features from file</source>
+      <translation>Importer les caractéristiques d'un fichier</translation>
+    </message>
+    <message>
+      <source>Import part</source>
+      <translation>Partie importation</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ImportPart:file_path:ExchangePlugin_ImportFormat</name>
+    <message>
+      <source>File name is empty.</source>
+      <translation>Le nom du fichier est vide.</translation>
+    </message>
+  </context>
+  <context>
+    <name>ImportPart:target_part</name>
+    <message>
+      <source>Import to</source>
+      <translation>Importation vers</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ImportMacro:target_part</name>
+    <message>
+      <source>Import to</source>
+      <translation>Importation vers</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ImportPart:file_path</name>
+    <message>
+      <source>Import file</source>
+      <translation>Fichier d'importation</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>workshop</name>
+    <message>
+      <source>Export part</source>
+      <translation>Partie exportation</translation>
+    </message>
+    <message>
+      <source>Import part</source>
+      <translation>Partie importation</translation>
+    </message>
+  </context>
+
+  <context>
+  <name>ImportMacro:file_path</name>
+    <message>
+      <source>Import file</source>
+      <translation>Fichier d'importation</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ImportMacro:file_path:ExchangePlugin_ImportFormat</name>
+    <message>
+      <source>File name is empty.</source>
+      <translation>Le nom du fichier est vide.</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ExportPart:file_path</name>
+    <message>
+      <source>Export file</source>
+      <translation>Fichier d'exportation</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ExportPart:file_path:ExchangePlugin_ExportFormat</name>
+    <message>
+      <source>File name is empty.</source>
+      <translation>Le nom du fichier est vide.</translation>
+    </message>
+  </context>
+
+  <context>
+    <name>ExportPart:selection_list</name>
+    <message>
+      <source>Select features or results</source>
+      <translation>Sélectionner les caractéristiques ou les résultats</translation>
+    </message>
+  </context>
 </TS>
