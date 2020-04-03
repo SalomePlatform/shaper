846fc649eb3451d1d2d6910d075d854a38c62039
Artem Zhidkov <Artem.Zhidkov@opencascade.com>
2020-04-03 22:21:33 +0300

    Fix regression related to issue #18994.

--- src/PartSetPlugin/PartSetPlugin_Remove.cpp
+++ src/PartSetPlugin/PartSetPlugin_Remove.cpp
@@ -47,7 +47,8 @@ void PartSetPlugin_Remove::execute()
       aRoot->refsToFeature(aFeature, aRefFeatures);
       if (aRefFeatures.empty()) {
         // before removing the part make the partSet active document
-        aPManager->setActiveDocument(aRoot);
+        if (aThisDoc == aPManager->activeDocument())
+          aPManager->setActiveDocument(aRoot);
         aRoot->removeFeature(aFeature);
         // the redisplay signal should be flushed in order to erase the feature presentation
         // in the viewer after removeFeature from the document
