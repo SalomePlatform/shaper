MrZhuNi/salome_shape

Issue #19725: Error when loading python dump
Fix the issue related to the on-the-fly projection to sketch, when projecting a periodic B-spline curve orthogonal to the sketch plane.

src/SketchPlugin/SketchPlugin_Validators.cpp
@@ -62,6 +62,7 @@
 #include <GeomAlgoAPI_EdgeBuilder.h>
 #include <GeomAlgoAPI_ShapeTools.h>
 
+#include <GeomAPI_BSpline.h>
 #include <GeomAPI_Circ.h>
 #include <GeomAPI_Dir2d.h>
 #include <GeomAPI_Ellipse.h>
@@ -1245,6 +1246,29 @@ bool SketchPlugin_ProjectionValidator::isValid(const AttributePtr& theAttribute,
     if (!aValid)
       theError.arg(anEdge->isClosed() ? "Error: Ellipse is orthogonal to the sketch plane."
                                       : "Error: Elliptic Arc is orthogonal to the sketch plane.");
+  }
+  else if (anEdge->isBSpline()) {
+    // check B-spline is periodic and planar
+    std::shared_ptr<GeomAPI_Curve> aCurve(new GeomAPI_Curve(anEdge));
+    std::shared_ptr<GeomAPI_BSpline> aBSpline(new GeomAPI_BSpline(aCurve));
+    if (aBSpline->isPeriodic()) {
+      GeomPlanePtr aPlane = GeomAlgoAPI_ShapeTools::findPlane(ListOfShape(1, anEdge));
+      std::shared_ptr<GeomAPI_Dir> aBSplineNormal = aPlane->direction();
+      double aDot = fabs(aNormal->dot(aBSplineNormal));
+      aValid = fabs(aDot - 1.0) <= tolerance * tolerance;
+      if (!aValid) {
+        // B-spline's plane is orthogonal to the sketch plane,
+        // thus, need to check whether B-spline is planar.
+        std::list<GeomPointPtr> aPoles = aBSpline->poles();
+        for (std::list<GeomPointPtr>::iterator it = aPoles.begin();
+             it != aPoles.end() && !aValid; ++it) {
+          if (aPlane->distance(*it) > tolerance)
+            aValid = true; // non-planar B-spline curve
+        }
+        if (!aValid)
+          theError = "Error: Periodic B-spline is orthogonal to the sketch plane.";
+      }
+    }
   }
 
   return aValid;

SalomePlatform/shaper

src/SketchPlugin/SketchPlugin_Validators.cpp
@@ -62,6 +62,7 @@
 #include <GeomAlgoAPI_EdgeBuilder.h>
 #include <GeomAlgoAPI_ShapeTools.h>
 
+#include <GeomAPI_BSpline.h>
 #include <GeomAPI_Circ.h>
 #include <GeomAPI_Dir2d.h>
 #include <GeomAPI_Ellipse.h>
@@ -1245,6 +1246,31 @@ bool SketchPlugin_ProjectionValidator::isValid(const AttributePtr& theAttribute,
     if (!aValid)
       theError.arg(anEdge->isClosed() ? "Error: Ellipse is orthogonal to the sketch plane."
                                       : "Error: Elliptic Arc is orthogonal to the sketch plane.");
+  }
+  else if (anEdge->isBSpline()) {
+    // check B-spline is periodic and planar
+    std::shared_ptr<GeomAPI_Curve> aCurve(new GeomAPI_Curve(anEdge));
+    std::shared_ptr<GeomAPI_BSpline> aBSpline(new GeomAPI_BSpline(aCurve));
+    if (aBSpline->isPeriodic()) {
+      GeomPlanePtr aBSplinePlane = GeomAlgoAPI_ShapeTools::findPlane(ListOfShape(1, anEdge));
+      if (aBSplinePlane) {
+        std::shared_ptr<GeomAPI_Dir> aBSplineNormal = aBSplinePlane->direction();
+        double aDot = fabs(aNormal->dot(aBSplineNormal));
+        aValid = fabs(aDot - 1.0) <= tolerance * tolerance;
+        if (!aValid) {
+          // B-spline's plane is orthogonal to the sketch plane,
+          // thus, need to check whether B-spline is planar.
+          std::list<GeomPointPtr> aPoles = aBSpline->poles();
+          for (std::list<GeomPointPtr>::iterator it = aPoles.begin();
+            it != aPoles.end() && !aValid; ++it) {
+            if (aBSplinePlane->distance(*it) > tolerance)
+              aValid = true; // non-planar B-spline curve
+          }
+          if (!aValid)
+            theError = "Error: Periodic B-spline is orthogonal to the sketch plane.";
+        }
+      }
+    }
   }
 
   return aValid;