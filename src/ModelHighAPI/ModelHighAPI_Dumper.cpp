0040e68ca340c8651eca54edee027a1928524df7
Artem Zhidkov <Artem.Zhidkov@opencascade.com>
2020-09-21 23:28:37 +0300

    Adjust the dumper to Python to avoid spaces at the end of lines.

--- src/ModelHighAPI/ModelHighAPI_Dumper.cpp
+++ src/ModelHighAPI/ModelHighAPI_Dumper.cpp
@@ -1505,12 +1505,12 @@ ModelHighAPI_Dumper& ModelHighAPI_Dumper::operator<<(
       }
 
       if(isAdded) {
-        *myDumpStorage << ", ";
         // print each attribute on separate line with the appropriate shift
         if (aNbSpaces > 0) {
           std::string aSpaces(aNbSpaces + 1, ' ');
-          *myDumpStorage << "\n" << aSpaces;
-        }
+          *myDumpStorage << ",\n" << aSpaces;
+        } else
+          *myDumpStorage << ", ";
       } else {
         isAdded = true;
       }
