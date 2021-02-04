c699b7f0efceb9e571ee6229124ad1da2e1f631d
jfa <jfa@opencascade.com>
2021-02-04 16:29:52 +0300

    bos #20600 EDF 22741 - export XAO

--- src/ExchangePlugin/doc/examples/export.py
+++ src/ExchangePlugin/doc/examples/export.py
@@ -9,5 +9,9 @@ Part_1_doc = Part_1.document()
 Box_1 = model.addBox(Part_1_doc, 10, 10, 10)
 Export_1 = model.exportToFile(Part_1_doc, file_path,
                               [model.selection("SOLID", "Box_1_1")])
+
+file_xao = os.path.join(os.getenv("DATA_DIR"), "test.xao")
+Export_2 = model.exportToXAO(Part_1_doc, file_xao,
+                             model.selection("SOLID", "Box_1_1"), "author", "box")
 model.do()
 model.end()
