cde0b40e898ae6fcccda1c5ac7f715c047a51efe
Artem Zhidkov <Artem.Zhidkov@opencascade.com>
2021-02-16 10:30:13 +0300

    Use the latest version of SALOME (2021-02-16)

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-export SALOME_DIR=/dn46/SALOME/series9x/current-2021-02-12
+export SALOME_DIR=/dn46/SALOME/series9x/current-2021-02-16
 
 # Path to sources
 export SOURCES_DIR=$(pwd)
