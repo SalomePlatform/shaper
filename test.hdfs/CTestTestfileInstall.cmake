5c2af839616d896f9d72561aef032435d56824d1
Anthony Geay <anthony.geay@edf.fr>
2021-08-23 15:13:44 +0200

    [tuleap24729] : rollback. BearingSeparator is OK but still use server launching.

--- test.hdfs/CTestTestfileInstall.cmake
+++ test.hdfs/CTestTestfileInstall.cmake
@@ -20,7 +20,7 @@
 INCLUDE(tests.set)
 
 SET(COMPONENT_NAME     SHAPER)
-SET(PYTHON_TEST_DRIVER "$ENV{KERNEL_ROOT_DIR}/bin/salome/appliskel/python_test_driver.py")
+SET(PYTHON_TEST_DRIVER "testme.py")
 SET(TIMEOUT            600)
 
 FOREACH(tfile ${PUBLIC_TESTS})
