21217df94b56054848cee8f215d2e73e5fc79ce4
mpv <mpv@opencascade.com>
2022-01-18 11:27:51 +0300

    Minor corrections for the user's guide.

--- src/FeaturesPlugin/doc/examples/create_normal_to_face.py
+++ src/FeaturesPlugin/doc/examples/create_normal_to_face.py
@@ -1,11 +1,12 @@
 from salome.shaper import model
 import os
 
+### Create a Box
 model.begin()
 Box_1 = model.addBox(Part_1_doc, 10, 10, 10)
 model.do()
-    
-### Create BoundingBox
+
+### Create a top face of the box normal
 Normal_1 = model.getNormal(Part_1_doc, model.selection("FACE", "Box_1_1/Top"))
 model.do()
 
